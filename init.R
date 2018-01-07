5a81d6f737e2f70f69be7c9a17b23b560a556e45
kurt.sys <kurt.sys@moment-4.be>
2018-01-07 22:40:23 +0100

    add Aptfile

--- init.R
+++ init.R
@@ -1,9 +1,19 @@
-# add packages ----
+# setup wd ----
+wd<-if(Sys.getenv("ON_HEROKU", unset=F)) {
+   Sys.getenv("APP_DIR", unset="/app")
+} else {
+   "~/dev/cryptominded/pricebot-graphs"
+}
+setwd(wd)
+
+
+# init libs ----
 if (!require("pacman")) install.packages("pacman")
 # init.R
 pacman::p_load("plumber")
 # api.R
 pacman::p_load("curl", "jsonlite", "dplyr", "xts", "zoo", "ggplot2", "ggExtra", "tidyquant", "urltools")
+
 
 # start app ----
 wd<-if(Sys.getenv("ON_HEROKU", unset=F)) {
