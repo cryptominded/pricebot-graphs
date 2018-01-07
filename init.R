53bef04ac62892dc000ce3c7d5ec0538772288dc
kurt.sys <kurt.sys@moment-4.be>
2018-01-07 21:00:37 +0100

    print wd

--- init.R
+++ init.R
@@ -1,14 +1,16 @@
 if (!require("pacman")) install.packages("pacman")
-pacman::p_load("plumber", "here")
+pacman::p_load("plumber")
 
 wd<-if(Sys.getenv("ON_HEROKU", unset=F)) {
    Sys.getenv("APP_DIR", unset="/app")
 } else {
    "~/dev/cryptominded/pricebot-graphs"
 }
+print(wd)
+
 setwd(wd)
 
-print(list.files(path=wd, pattern="*.R$", recursive=T))
+print(list.files(path=wd, pattern="*.R$"))
 
 r <- plumb(file=paste(wd, "api.R", sep="/"))
 
