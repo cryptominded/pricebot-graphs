f61f38e803b198a6530dd942893effd932de7033
kurt.sys <kurt.sys@moment-4.be>
2018-01-07 15:36:38 +0100

    config port and wd

--- init.R
+++ init.R
@@ -1,7 +1,15 @@
 if (!require("pacman")) install.packages("pacman")
 pacman::p_load("plumber")
 
+wd<-if(Sys.getenv("ON_HEROKU", unset=F)) {
+   Sys.getenv("HOME", unset="~/dev/cryptominded/pricebot-graphs")
+} else {
+   "~/dev/cryptominded/pricebot-graphs"
+}
+print(wd)
+setwd(wd)
+rm(wd)
 
 r <- plumb("api.R")  
 
-r$run(port=3333, swagger=TRUE)
+r$run(port=as.numeric(Sys.getenv("PORT", unset=3333)), swagger=TRUE)
