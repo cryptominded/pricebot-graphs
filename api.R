94c19d9eb9653985128bc3aa9979baa08c047cb1
kurt.sys <kurt.sys@moment-4.be>
2018-01-11 21:58:24 +0100

    update graphs, large text

--- api.R
+++ api.R
@@ -4,11 +4,14 @@ lapply(c("rJava", "curl", "jsonlite", "dplyr", "xts", "zoo", "ggplot2", "ggExtra
        require,
        character.only=T)
 
+if(Sys.getenv("ON_HEROKU", unset=F)) {
+   .jinit()
+   .jaddClassPath("/app")
+   .jclassPath()
+}
+
 pricedata<-dget("pricedata.R")()
 
-.jinit()
-.jaddClassPath("/app")
-.jclassPath()
 
 #* @apiTitle Some title
 #* @apiDescription Description 
@@ -29,7 +32,7 @@ testgraph <- function() {
 #' @param tsym:character symbol
 #' @param period:character period
 #' @get /graph
-#' @png()
+#' @png(width=777,height=480)
 #' @response 400 Some error...
 #' @response 404 I have been looking very deeply, but I can't find what you ask me
 graph <- function(fsym="BTC", tsym="USD", period="1day") {
@@ -39,12 +42,12 @@ graph <- function(fsym="BTC", tsym="USD", period="1day") {
       geom_barchart(aes(open=open, high=high, low=low, close=close),
                     color_up="green", color_down="red") +
       geom_point(aes(y=avg), cex=0.3) +
-      labs(x="date&time") +
+      labs(x="date&time",y=paste0("price (", tsym, "/1 ", fsym, ")")) +
       ggtitle(paste(fsym, tsym, period, sep=" - ")) +
       #geom_ma(aes(volume=volumefrom),
       #        ma_fun=VWMA, n=13, colour="blue", size=0.3) +
       #geom_ma(ma_fun=EMA, n=13, colour="orange", size=0.3) +
-      theme_tq()
+      theme_tq(base_size=25)
    
    
    print(pl)
